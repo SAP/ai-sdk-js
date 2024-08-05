f9d2296371cff7be09be31d1bece7a3e2f6f33c1
Kuhr <matthias.kuhr@sap.com>
2024-08-05 18:21:36 +0200

    Add AI Core and Orchestration Tests

---
+++ tests/e2e-tests/src/orchestration.test.ts
@@ -0,0 +1,21 @@
+import { GenAiHubClient, GenAiHubCompletionParameters } from '@sap-ai-sdk/gen-ai-hub';
+import 'dotenv/config';
+
+describe('orchestration', () => {
+  it('should complete a chat', async () => {
+    const request: GenAiHubCompletionParameters = {
+      deploymentConfiguration: { deploymentId: 'db1d64d9f06be467' },
+      llmConfig: {
+        model_name: 'gpt-35-turbo-16k',
+        model_params: { max_tokens: 50, temperature: 0.1 }
+      },
+      prompt: {
+        template: [{ role: 'user', content: 'Hello!' }]
+      }
+    };
+    const response = await new GenAiHubClient().chatCompletion(request);
+    
+    expect(response.module_results).toBeDefined();
+    expect(response.orchestration_result.choices).not.toHaveLength(0);
+  });
+});
