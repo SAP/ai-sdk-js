f44467441d0a385a69e6ea92c1beccc32bfa2a1a
Kuhr <matthias.kuhr@sap.com>
2024-08-05 18:46:18 +0200

    Disable orchestration e2e test

--- tests/e2e-tests/src/orchestration.test.ts
+++ tests/e2e-tests/src/orchestration.test.ts
@@ -2,7 +2,7 @@ import { GenAiHubClient, GenAiHubCompletionParameters } from '@sap-ai-sdk/gen-ai
 import 'dotenv/config';
 
 describe('orchestration', () => {
-  it('should complete a chat', async () => {
+  test.skip('should complete a chat, but currently doesn\'t work', async () => {
     const request: GenAiHubCompletionParameters = {
       deploymentConfiguration: { deploymentId: 'db1d64d9f06be467' },
       llmConfig: {
