52d42a13c351c6d79daecd47e655bd769f8e6ed1
Tom Frenken <tom.frenken@sap.com>
2024-08-09 11:47:41 +0200

    lint

--- packages/gen-ai-hub/src/client/openai/openai-client.test.ts
+++ packages/gen-ai-hub/src/client/openai/openai-client.test.ts
@@ -1,11 +1,11 @@
 import nock from 'nock';
 import { jest } from '@jest/globals';
 import { HttpDestination } from '@sap-cloud-sdk/connectivity';
-import { mockGetAiCoreDestination } from '../../../../../test-util/mock-context.js';
 import {
   BaseLlmParametersWithDeploymentId
 } from '@sap-ai-sdk/core';
 import { EndpointOptions } from '@sap-ai-sdk/core/src/http-client.js';
+import { mockGetAiCoreDestination } from '../../../../../test-util/mock-context.js';
 import { mockInference, parseMockResponse } from '../../../../../test-util/mock-http.js';
 import {
   OpenAiChatCompletionOutput,
