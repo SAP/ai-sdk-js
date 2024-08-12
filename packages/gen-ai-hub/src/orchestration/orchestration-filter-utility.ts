cc8cc087e217f27ff8bf68c00bb9388a91b6fb71
KavithaSiva <kavitha.sivakumar@sap.com>
2024-08-12 13:47:14 +0200

    chore: improve JSDoc

--- packages/gen-ai-hub/src/orchestration/orchestration-filter-utility.ts
+++ packages/gen-ai-hub/src/orchestration/orchestration-filter-utility.ts
@@ -2,7 +2,7 @@ import { AzureContentSafety, FilteringConfig } from './client/api/index.js';
 
 /**
  * Convenience function to create Azure filters.
- * @param filter - Filtering configuration for Azure filter.
+ * @param filter - Filtering configuration for Azure filter. If skipped, the default Azure filter configuration is used.
  * @returns An object with the Azure filtering configuration.
  */
 export function azureContentFilter(
