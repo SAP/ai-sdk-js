88f08014025028f6ebd826881b83ddcc7d97540a
Tom Frenken <tom.frenken@sap.com>
2024-08-13 14:25:06 +0200

    update docs

--- global-test-setup.ts
+++ global-test-setup.ts
@@ -1,4 +1,7 @@
-export default async function mockAiCoreEnvVariable() {
+/**
+ * This file is used to mock the environment variables that are required for the tests.
+ */
+export default async function mockAiCoreEnvVariable(): Promise<void> {
     const aiCoreServiceCredentials =  {
         clientid: 'clientid',
         clientsecret: 'clientsecret',
@@ -6,6 +9,6 @@ export default async function mockAiCoreEnvVariable() {
         serviceurls: {
           AI_API_URL: 'https://api.ai.ml.hana.ondemand.com'
         }
-      }
+      };
     process.env['aicore'] = JSON.stringify(aiCoreServiceCredentials);
 }
--- global-test-teardown.ts
+++ global-test-teardown.ts
@@ -1,3 +1,6 @@
-export default async function tearDown() {
+/**
+ * This file is used to run code after all tests have been run.
+ */
+export default async function tearDown(): Promise<void> {
     delete process.env.aicore;
 }
