c201807edcedf2d46f740b4bbb0399a3e77fd9af
KavithaSiva <kavitha.sivakumar@sap.com>
2024-08-16 10:50:43 +0200

    fix: e2e test .env path

--- tests/e2e-tests/src/ai-core.test.ts
+++ tests/e2e-tests/src/ai-core.test.ts
@@ -1,7 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 import { DeploymentApi } from '@sap-ai-sdk/ai-core';
 import { getAiCoreDestination } from '@sap-ai-sdk/core';
 import { HttpDestination } from '@sap-cloud-sdk/connectivity';
 import 'dotenv/config';
+
+// Pick .env file from root directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 describe('ai-core', () => {
   test.skip('should get deployments, but is currently broken', async () => {
--- tests/e2e-tests/src/orchestration.test.ts
+++ tests/e2e-tests/src/orchestration.test.ts
@@ -1,8 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 import {
   GenAiHubClient,
   GenAiHubCompletionParameters
 } from '@sap-ai-sdk/gen-ai-hub';
-import 'dotenv/config';
+
+// Pick .env file from root directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
 describe('orchestration', () => {
   it('should complete a chat', async () => {
