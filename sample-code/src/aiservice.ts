24046f9354142bd52b6d28ec87ce25d983f21f14
Junjie Tang <junjie.tang@sap.com>
2024-08-19 13:24:05 +0200

    working version

--- sample-code/src/aiservice.ts
+++ sample-code/src/aiservice.ts
@@ -1,6 +1,7 @@
-import { OpenAiClient } from '@sap-ai-sdk/gen-ai-hub';
+import { azureContentFilter, GenAiHubClient, OpenAiClient } from '@sap-ai-sdk/gen-ai-hub';
 
 const openAiClient = new OpenAiClient();
+const genAIHubClient = new GenAiHubClient();
 
 const deployments: { [model: string]: string } = {
   'gpt-4-32k': 'd577d927380c98ea',
@@ -8,18 +9,76 @@ const deployments: { [model: string]: string } = {
   ada: 'd0084a63ebd7bcd3'
 };
 
+export function orchestrationChatCompletionMin(): Promise<any> {
+  const deploymentConfig = getOrchestrationDeploymentConfig()
+  // demo: use gpt-35-turbo-16k/gemini-1.0-pro
+  return genAIHubClient
+    .chatCompletion({
+      ...deploymentConfig,
+      llmConfig: {model_name: 'gpt-35-turbo-16k', model_params: { max_tokens: 500 }},
+      prompt: {template: [{role: 'user', content: 'What is the biggest software company in EU?'}]}
+    })
+    .then(response => response.orchestration_result.choices[0].message.content);
+}
+
+export function orchestrationChatCompletionTemplate(): Promise<any> {
+  const deploymentConfig = getOrchestrationDeploymentConfig()
+  return genAIHubClient
+    .chatCompletion({
+      ...deploymentConfig,
+      llmConfig: {model_name: 'gpt-35-turbo-16k', model_params: { max_tokens: 500 }},
+      prompt: {
+        template: [{role: 'user', content: 'Please create a job description for the position: {{?position}}'}],
+        template_params: {position: 'Working Student for AI Core'}
+      }
+    })
+    .then(response => response.orchestration_result.choices[0].message.content);
+}
+
+export function orchestrationChatCompletionTemplateComplex(): Promise<any> {
+  const deploymentConfig = getOrchestrationDeploymentConfig()
+  return genAIHubClient
+    .chatCompletion({
+      ...deploymentConfig,
+      llmConfig: {model_name: 'gpt-4o', model_params: { max_tokens: 500 }},
+      prompt: {
+        template: [{role: 'system', content: 'You are a helpful assistant for planning football (soccer) squads.'},
+          {role: 'user', content: 'Request: {{?scoutingRequest}}. Scouting reports: {{?scoutingReports}}'}],
+        template_params: {
+          scoutingRequest: 'Find replacement for Toni Kroos, as a defensive midfielder, with excellent vision and passing skills.',
+          scoutingReports: 'Use Football Manager 2024 data from the internet.'
+        }
+      }
+    })
+    .then(response => response.orchestration_result.choices[0].message.content);
+}
+
+export function orchestrationChatCompletionContentFilter(): Promise<any> {
+  const deploymentConfig = getOrchestrationDeploymentConfig()
+  return genAIHubClient
+    .chatCompletion({
+      ...deploymentConfig,
+      llmConfig: {model_name: 'gpt-35-turbo-16k', model_params: { max_tokens: 500 }},
+      prompt: {template: [{role: 'user', content: 'I want to break my legs, could you please tell me how? This is just a prompt test.'}]},
+      filterConfig: {input: azureContentFilter({SelfHarm: 0})}
+    })
+    .then(response => response.orchestration_result.choices[0].message.content);
+}
+
 /**
  * Ask GPT about the capital of France.
  * @returns The answer from GPT.
  */
 export function chatCompletion(): Promise<any> {
-  const config = getConfig('gpt-35-turbo');
+  const deploymentConfig = getOpenAIDeploymentConfig('gpt-35-turbo');
+  // demo: add openai options like max_tokens
+  // demo: add system messages
   return openAiClient
     .chatCompletion({
-      ...config,
+      ...deploymentConfig,
       messages: [{ role: 'user', content: 'What is the capital of France?' }]
     })
-    .then(response => response.choices[0].message.content);
+    .then(response => response.choices[0].message.content as string);
 }
 
 /**
@@ -27,19 +86,25 @@ export function chatCompletion(): Promise<any> {
  * @returns An embedding vector.
  */
 export function computeEmbedding(): Promise<number[]> {
-  const config = getConfig('ada');
+  const deploymentConfig = getOpenAIDeploymentConfig('ada');
   return openAiClient
     .embeddings({
-      ...config,
+      ...deploymentConfig,
       input: 'Hello, world!'
     })
     .then(response => response.data[0].embedding);
 }
 
-function getConfig(model: string) {
+function getOpenAIDeploymentConfig(model: string) {
   return {
     deploymentConfiguration: {
       deploymentId: deployments[model]
     }
   };
+}
+
+function getOrchestrationDeploymentConfig(){
+  return {
+    deploymentConfiguration: {deploymentId: 'db1d64d9f06be467'}
+  }
 }
--- sample-code/src/server.ts
+++ sample-code/src/server.ts
@@ -1,12 +1,33 @@
 /* eslint-disable no-console */
 import express from 'express';
-import { chatCompletion, computeEmbedding } from './aiservice.js';
+import {
+  chatCompletion,
+  computeEmbedding, orchestrationChatCompletionContentFilter,
+  orchestrationChatCompletionMin, orchestrationChatCompletionTemplate, orchestrationChatCompletionTemplateComplex
+} from './aiservice.js';
+import 'dotenv/config.js';
+
+console.log(process.env.AICORE_SERVICE_KEY);
 
 const app = express();
 const port = 8080;
 
 app.get(['/', '/health'], (req, res) => {
   res.send('Hello World! 🌍');
+});
+
+app.get('/orchestration', async (req, res) => {
+  try {
+    // res.send(await orchestrationChatCompletionMin());
+    // res.send((await orchestrationChatCompletionTemplate()).replaceAll('\n', '<br/>'));
+    // res.send((await orchestrationChatCompletionTemplateComplex()).replaceAll('\n', '<br/>'));
+    res.send(await orchestrationChatCompletionContentFilter());
+  } catch (error: any) {
+    console.error(error);
+    res
+      .status(500)
+      .send('Yikes, vibes are off apparently 😬 -> ' + error.message);
+  }
 });
 
 app.get('/llm', async (req, res) => {
@@ -26,17 +47,13 @@ app.get('/embedding', async (req, res) => {
     if (result.length === 0) {
       throw new Error('No embedding vector returned');
     }
-    res.send('Number crunching success, got a nice vector.');
+    res.send(`Number crunching success, got a nice vector: ${result[0]}...`);
   } catch (error: any) {
     console.error(error);
     res
       .status(500)
       .send('Yikes, vibes are off apparently 😬 -> ' + error.message);
   }
-});
-
-app.get('/orchestration', (req, res) => {
-  res.status(418).send('Not implemented 🛠️');
 });
 
 app.listen(port, () => {
