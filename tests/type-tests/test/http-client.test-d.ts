4458fe4bfce9af8508a2e8b420dc8ff3bffac803
Kuhr <matthias.kuhr@sap.com>
2024-08-19 11:11:43 +0200

    Fix type test

--- tests/type-tests/test/http-client.test-d.ts
+++ tests/type-tests/test/http-client.test-d.ts
@@ -5,15 +5,7 @@ import { executeRequest } from '@sap-ai-sdk/core';
 expectType<Promise<HttpResponse>>(
   executeRequest(
     { url: 'https://example.com', apiVersion: 'v1' },
-    { deploymentConfiguration: { deploymentId: 'id' }, prompt: 'test prompt' },
     { headers: { 'Content-Type': 'application/json' } }
-  )
-);
-
-expectError<any>(
-  executeRequest(
-    { url: 'https://example.com', apiVersion: 'v1' },
-    { prompt: 'test prompt' }
   )
 );
 
