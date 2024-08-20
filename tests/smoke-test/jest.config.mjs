d515925f122d9a5fa04b40d1fc3cd36efd113534
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-08-20 12:09:20 +0200

    fix config

--- tests/smoke-test/jest.config.mjs
+++ tests/smoke-test/jest.config.mjs
@@ -1,5 +1,15 @@
 import config from '../../jest.config.mjs';
 export default {
   ...config,
+  globalSetup: undefined,
+  globalTeardown: undefined,
   displayName: 'smoke-test',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
