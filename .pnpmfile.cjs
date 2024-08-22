257acf0584acd3463bf9a614bf9a588afa567386
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-08-22 10:47:38 +0200

    make script more generic

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -7,12 +7,11 @@ module.exports = {
           delete lockfile.packages[pkg];
         });
 
-      delete lockfile.importers['tests/smoke-tests'].dependencies[
-        '@sap-ai-sdk/ai-core'
-      ];
-      delete lockfile.importers['tests/smoke-tests'].dependencies[
-        '@sap-ai-sdk/gen-ai-hub'
-      ];
+      Object.keys(lockfile.importers['tests/smoke-tests'].dependencies)
+        .filter(pkg => pkg.startsWith('@sap-ai-sdk'))
+        .forEach(pkg => {
+          delete lockfile.importers['tests/smoke-tests'].dependencies[pkg];
+        });
 
       return lockfile;
     }
