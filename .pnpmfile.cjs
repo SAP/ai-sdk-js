a5a7224efbbd6961b3f30210867fd4c84f5a08fe
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-08-26 17:08:17 +0200

    do not adjust lockfile

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   hooks: {
     afterAllResolved: async lockfile => {
-      Object.keys(lockfile.packages)
-        .filter(pkg => pkg.startsWith('@sap-ai-sdk'))
-        .forEach(pkg => {
-          delete lockfile.packages[pkg];
-        });
+      // Object.keys(lockfile.packages)
+      //   .filter(pkg => pkg.startsWith('@sap-ai-sdk'))
+      //   .forEach(pkg => {
+      //     delete lockfile.packages[pkg];
+      //   });
 
-      Object.keys(lockfile.importers['tests/smoke-tests'].dependencies)
-        .filter(pkg => pkg.startsWith('@sap-ai-sdk'))
-        .forEach(pkg => {
-          delete lockfile.importers['tests/smoke-tests'].dependencies[pkg];
-        });
+      // Object.keys(lockfile.importers['tests/smoke-tests'].dependencies)
+      //   .filter(pkg => pkg.startsWith('@sap-ai-sdk'))
+      //   .forEach(pkg => {
+      //     delete lockfile.importers['tests/smoke-tests'].dependencies[pkg];
+      //   });
 
       return lockfile;
     }
