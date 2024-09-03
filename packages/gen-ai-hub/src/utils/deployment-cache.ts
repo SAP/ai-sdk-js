37eaadd614448bcbaaee909dda48a1032c89f0d4
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-09-03 18:15:18 +0200

    implement caching without full model explicitly

--- packages/gen-ai-hub/src/utils/deployment-cache.ts
+++ packages/gen-ai-hub/src/utils/deployment-cache.ts
@@ -1,5 +1,6 @@
 import { Cache } from '@sap-cloud-sdk/connectivity/internal.js';
 import { type AiDeployment } from '@sap-ai-sdk/ai-core';
+import { unique } from '@sap-cloud-sdk/util';
 import { type DeploymentResolutionOptions } from './deployment-resolver.js';
 import { extractModel, type FoundationModel } from './model.js';
 
@@ -54,27 +55,67 @@ function createDeploymentCache(cache: Cache<Deployment>) {
       opts: DeploymentResolutionOptions,
       deployments: AiDeployment[]
     ): void => {
-      // go backwards to cache the first deployment ID for each model
-      [...deployments].reverse().forEach(deployment => {
-        const model = extractModel(deployment);
-        const entry = transformDeploymentForCache(deployment);
-        cache.set(getCacheKey({ ...opts, model }), {
+      const cacheEntries = getUniqueCacheEntries(
+        [...deployments].map(deployment =>
+          transformDeploymentForCache(deployment)
+        )
+      );
+
+      [
+        ...cacheEntries,
+        ...getCacheEntriesForModelsWithoutVersion(cacheEntries),
+        ...getCacheEntriesForNoModel(cacheEntries)
+      ].forEach(entry => {
+        cache.set(getCacheKey({ ...opts, model: entry.model }), {
           entry
         });
-
-        if (model) {
-          cache.set(getCacheKey({ ...opts, model: undefined }), { entry });
-          if (model.version) {
-            cache.set(
-              getCacheKey({ ...opts, model: { ...model, version: undefined } }),
-              { entry }
-            );
-          }
-        }
       });
     },
     clear: () => cache.clear()
   };
+}
+
+function getUniqueCacheEntries(cacheEntries: Deployment[]): Deployment[] {
+  const modelMap: Record<string, string[]> = {};
+  const modelNames = unique(cacheEntries.map(({ model }) => model?.name ?? ''));
+  modelNames.forEach(modelName => {
+    modelMap[modelName] = unique(
+      cacheEntries.map(({ model }) => model?.version ?? '')
+    );
+  });
+
+  return modelNames.flatMap(modelName =>
+    modelMap[modelName].map(
+      modelVersion =>
+        cacheEntries.find(
+          entry =>
+            entry.model?.name === modelName &&
+            entry.model?.version === modelVersion
+        )!
+    )
+  );
+}
+
+function getCacheEntriesForModelsWithoutVersion(
+  cacheEntries: Deployment[]
+): Deployment[] {
+  const modelNames = unique(
+    cacheEntries
+      .filter(({ model }) => model?.name && model.version)
+      .map(({ model }) => model?.name as string)
+  );
+
+  return modelNames.map(modelName => ({
+    // cannot be undefined
+    id: cacheEntries.find(({ model }) => model?.name === modelName)!.id,
+    model: { name: modelName, version: undefined }
+  }));
+}
+
+function getCacheEntriesForNoModel(cacheEntries: Deployment[]): Deployment[] {
+  return cacheEntries[0]?.model
+    ? [{ ...cacheEntries[0], model: undefined }]
+    : [];
 }
 
 function transformDeploymentForCache(deployment: AiDeployment): Deployment {
