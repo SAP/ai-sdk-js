f11135842c4f43f2475470c1c816d807340de79a
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-09-05 11:58:53 +0200

    “fix” cjs check

--- packages/ai-core/src/client/index.ts
+++ packages/ai-core/src/client/index.ts
@@ -1,2 +1,4 @@
 // This file is only needed for the public API check
 // It will be overwritten when regenerating, we will need a better solution soon
+// Also, if there is nothing in this file, the CJS check fails, therefore the dummy
+export const dummy = 'dummy';
