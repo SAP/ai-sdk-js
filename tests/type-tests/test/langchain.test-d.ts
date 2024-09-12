f28693ac17fb01fadd567ac256abbab9e7b93c0f
cloud-sdk-js <cloud-sdk-js@github.com>
2024-09-12 11:37:20 +0000

    fix: Changes from lint

--- tests/type-tests/test/langchain.test-d.ts
+++ tests/type-tests/test/langchain.test-d.ts
@@ -4,29 +4,29 @@ import { AIMessageChunk } from '@langchain/core/messages';
 import { LLMResult } from '@langchain/core/outputs';
 
 expectError(
-    new OpenAiChatClient({ deploymentId: 'test', modelName: 'test' }).invoke('Test')
+  new OpenAiChatClient({ deploymentId: 'test', modelName: 'test' }).invoke(
+    'Test'
+  )
 );
 
-expectError(
-    new OpenAiChatClient({ modelName: 'my-cool-chat-model' })
-);
+expectError(new OpenAiChatClient({ modelName: 'my-cool-chat-model' }));
 
-expectError(
-    new OpenAiChatClient({ deploymentId: 'test', apiKey: 'test' })
-);
+expectError(new OpenAiChatClient({ deploymentId: 'test', apiKey: 'test' }));
 
 expectType<Promise<AIMessageChunk>>(
-    new OpenAiChatClient({ modelName: 'gpt-35-turbo' }).invoke('Test')
+  new OpenAiChatClient({ modelName: 'gpt-35-turbo' }).invoke('Test')
 );
 
 expectType<Promise<LLMResult>>(
-    new OpenAiChatClient({ modelName: 'gpt-35-turbo' }).generate([['Test']])
+  new OpenAiChatClient({ modelName: 'gpt-35-turbo' }).generate([['Test']])
 );
 
 expectType<Promise<number[]>>(
-    new OpenAiEmbeddingClient({ modelName: 'text-embedding-3-large' }).embedQuery('test')
+  new OpenAiEmbeddingClient({ modelName: 'text-embedding-3-large' }).embedQuery(
+    'test'
+  )
 );
 
 expectError(
-    new OpenAiEmbeddingClient({ modelName: 'my-cool-embedding-model' })
+  new OpenAiEmbeddingClient({ modelName: 'my-cool-embedding-model' })
 );
