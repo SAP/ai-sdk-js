43a4db817d2b786a0f176868a877083c71f2680d
Tom Frenken <tom.frenken@sap.com>
2024-09-13 17:41:57 +0200

    finish unit tests

--- packages/langchain/src/openai/util.test.ts
+++ packages/langchain/src/openai/util.test.ts
@@ -1,24 +1,69 @@
 // mapResponseToChatResult
 // mapLangchainToAiClient
 
-import { OpenAiChatCompletionOutput , OpenAiChatClient as OpenAiChatClientBase, OpenAiEmbeddingParameters } from '@sap-ai-sdk/foundation-models';
+import {  OpenAiChatClient as OpenAiChatClientBase, OpenAiChatCompletionOutput } from '@sap-ai-sdk/foundation-models';
 import { jest } from '@jest/globals';
 import nock from 'nock';
+import { HumanMessage } from '@langchain/core/messages';
 import { mockClientCredentialsGrantCall, mockInference, parseMockResponse } from '../../../../test-util/mock-http.js';
 import { mapResponseToChatResult } from './util.js';
 import { OpenAiChatClient } from './chat.js';
 
+const openAiMockResponse = parseMockResponse<OpenAiChatCompletionOutput>(
+  'foundation-models',
+  'openai-chat-completion-success-response.json'
+);
+
+const chatCompletionEndpoint = {
+  url: 'inference/deployments/1234/chat/completions',
+  apiVersion: '2024-02-01'
+};
+
+const prompt = {
+  messages: [
+    {
+      role: 'user',
+      content: 'Where is the deepest place on earth located',
+      tool_call_id: ''
+    }
+  ],
+  max_tokens: 256,
+  temperature: 0.7,
+  top_p: 1,
+  n: 1,
+  presence_penalty: 0,
+  frequency_penalty: 0
+};
+
+const langchainPrompt = new HumanMessage('Where is the deepest place on earth located');
+
+const request = {
+  frequency_penalty: 0,
+  functions: undefined,
+  logit_bias: undefined,
+  max_tokens: 256,
+  messages: [
+    {
+      content: 'Where is the deepest place on earth located',
+      function_call: undefined,
+      name: undefined,
+      role: 'user',
+      tool_call_id: '',
+      tool_calls: undefined
+    }
+  ],
+  n: 1,
+  presence_penalty: 0,
+  response_format: undefined,
+  seed: undefined,
+  stop: undefined,
+  temperature: 0.7,
+  tool_choice: undefined,
+  tools: undefined,
+  top_p: 1
+};
+
 describe('Mapping Functions', () => {
-  const openAiMockResponse = parseMockResponse<OpenAiChatCompletionOutput>(
-    'foundation-models',
-    'openai-chat-completion-success-response.json'
-  );
-
-  const chatCompletionEndpoint = {
-    url: 'inference/deployments/1234/chat/completions',
-    apiVersion: '2024-02-01'
-  };
-
   beforeEach(() => {
     mockClientCredentialsGrantCall();
   });
@@ -33,10 +78,6 @@ describe('Mapping Functions', () => {
   });
 
   it('should parse a Langchain input to an ai sdk input', async () => {
-    const prompt = {
-      input: ['AI is fascinating']
-    } as OpenAiEmbeddingParameters;
-
     mockInference(
       {
         data: prompt
@@ -50,8 +91,7 @@ describe('Mapping Functions', () => {
 
     const client = new OpenAiChatClient({ deploymentId: '1234' });
     const runSpy = jest.spyOn(OpenAiChatClientBase.prototype, 'run');
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const response = await client.invoke('Test');
-    expect(runSpy).toHaveBeenCalled();
+    await client.generate([[langchainPrompt]]);
+    expect(runSpy).toHaveBeenCalledWith(request);
   });
 });
