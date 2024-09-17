d82bbf762267cb2ca0880cc0b19e95562611e510
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-09-17 14:16:42 +0200

    remove uncommented code

--- scripts/update-imports.ts
+++ scripts/update-imports.ts
@@ -2,36 +2,6 @@
 import { basename, resolve } from 'node:path';
 import { transformFilesInDirectory } from './util.js';
 
-// async function processApiFile(filePath: string) {
-//   return transformFile(filePath, file =>
-//     file.replace(
-//       /import \{ OpenApiRequestBuilder \} from '@sap-cloud-sdk\/openapi';/g,
-//       "import { OpenApiRequestBuilder } from '@sap-ai-sdk/core';"
-//     )
-//   );
-// }
-
-// // Function to recursively traverse the directory and apply transformations
-// async function traverseDirectory(dirPath: string): Promise<void> {
-//   const files = await readdir(dirPath);
-
-//   for (const file of files) {
-//     const filePath = join(dirPath, file);
-
-//     try {
-//       const { isDirectory, isFile } = await stat(filePath);
-
-//       if (isDirectory() && basename(filePath) !== 'schema') {
-//         await traverseDirectory(filePath); // Recursive traversal for directories
-//       } else if (isFile() && file !== 'index.ts') {
-//         await processApiFile(filePath); // Process files named 'index.ts'
-//       }
-//     } catch (err) {
-//       console.error(`Error processing ${filePath}:`, err);
-//     }
-//   }
-// }
-
 // Entry point: Get the root directory from command-line arguments
 const rootDir = process.argv[2];
 
