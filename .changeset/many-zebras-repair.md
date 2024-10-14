09c16b43152db9b87704b93e2e2ae9c7acf3fcd1
Marika Marszalkowski <marika.marszalkowski@sap.com>
2024-10-14 17:00:46 +0200

    feat: Adjust embedding types and access

---
+++ .changeset/many-zebras-repair.md
@@ -0,0 +1,5 @@
+---
+'@sap-ai-sdk/foundation-models': minor
+---
+
+[Compatibility Note] Adjust `AzureOpenAiEmbeddingOutput` type to include multiple embedding responses as opposed to one.
--- packages/foundation-models/src/azure-openai/azure-openai-embedding-response.test.ts
+++ packages/foundation-models/src/azure-openai/azure-openai-embedding-response.test.ts
@@ -2,35 +2,57 @@ import { createLogger } from '@sap-cloud-sdk/util';
 import { jest } from '@jest/globals';
 import { parseMockResponse } from '../../../../test-util/mock-http.js';
 import { AzureOpenAiEmbeddingResponse } from './azure-openai-embedding-response.js';
+import type { HttpResponse } from '@sap-cloud-sdk/http-client';
+import type { AzureOpenAiEmbeddingOutput } from './azure-openai-embedding-types.js';
 
 describe('Azure OpenAI embedding response', () => {
-  const mockResponse = parseMockResponse<AzureOpenAiEmbeddingResponse>(
-    'foundation-models',
-    'azure-openai-embeddings-success-response.json'
-  );
-  const rawResponse = {
-    data: mockResponse,
-    status: 200,
-    headers: {},
-    request: {}
-  };
-  const embeddingResponse = new AzureOpenAiEmbeddingResponse(rawResponse);
+  let embeddingResponse: AzureOpenAiEmbeddingResponse;
+  let rawResponse: HttpResponse;
+  let mockedData: AzureOpenAiEmbeddingOutput;
+  beforeAll(async () => {
+    mockedData = await parseMockResponse<AzureOpenAiEmbeddingOutput>(
+      'foundation-models',
+      'azure-openai-embeddings-success-response.json'
+    );
+
+    rawResponse = {
+      data: mockedData,
+      status: 200,
+      headers: {},
+      request: {}
+    };
+    embeddingResponse = new AzureOpenAiEmbeddingResponse(rawResponse);
+  });
 
   it('should return the embedding response', () => {
-    expect(embeddingResponse.data).toStrictEqual(mockResponse);
+    expect(embeddingResponse.data).toStrictEqual(mockedData);
   });
 
   it('should return raw response', () => {
     expect(embeddingResponse.rawResponse).toBe(rawResponse);
   });
+
+  it('should return the first embedding', () => {
+    expect(embeddingResponse.getEmbedding()).toEqual(
+      mockedData.data[0].embedding
+    );
+  });
+
   it('should return undefined when convenience function is called with incorrect index', () => {
     const logger = createLogger({
       package: 'foundation-models',
       messageContext: 'azure-openai-embedding-response'
     });
     const errorSpy = jest.spyOn(logger, 'error');
-    expect(embeddingResponse.getEmbedding(1)).toBeUndefined();
-    expect(errorSpy).toHaveBeenCalledWith('Data index 1 is out of bounds.');
+    expect(embeddingResponse.getEmbedding(2)).toBeUndefined();
+    expect(errorSpy).toHaveBeenCalledWith('Data index 2 is out of bounds.');
     expect(errorSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return all embeddings', () => {
+    expect(embeddingResponse.getEmbeddings()).toEqual([
+      mockedData.data[0].embedding,
+      mockedData.data[1]?.embedding
+    ]);
   });
 });
