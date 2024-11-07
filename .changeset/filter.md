e73065896266e62880809ecd187ac324d0c789a0
Junjie Tang <junjie.tang@sap.com>
2024-11-07 14:33:49 +0100

    filter

---
+++ .changeset/filter.md
@@ -0,0 +1,6 @@
+---
+'@sap-ai-sdk/orchestration': minor
+---
+[Fixed Issue] Fix input and output filters to disallow additional properties as part of the filter config.
+
+[Compatibility Note] `FilterConfig` has been renamed to `InputFilterConfig` and `OutputFilterCOnfig` for future extensibility.
--- packages/orchestration/src/client/api/schema/filtering-module-config.ts
+++ packages/orchestration/src/client/api/schema/filtering-module-config.ts
@@ -4,6 +4,8 @@
  * This is a generated file powered by the SAP Cloud SDK for JavaScript.
  */
 import type { FilteringConfig } from './filtering-config.js';
+import { InputFilteringConfig } from './input-filtering-config.js';
+import { OutputFilteringConfig } from './output-filtering-config.js';
 /**
  * Representation of the 'FilteringModuleConfig' schema.
  */
@@ -11,9 +13,9 @@ export interface FilteringModuleConfig {
   /**
    * List of provider type and filters.
    */
-  input?: FilteringConfig;
+  input?: InputFilteringConfig;
   /**
    * List of provider type and filters.
    */
-  output?: FilteringConfig;
+  output?: OutputFilteringConfig;
 }
