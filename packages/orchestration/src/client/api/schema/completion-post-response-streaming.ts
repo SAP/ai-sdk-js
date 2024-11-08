eb7a4ec7f38c265edc700dd5687f6edbcaadac8f
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2024-11-08 14:06:20 +0100

    chore: Add new schemas for streaming (#277)
    * add new schemas
    
    * fix
    
    * fix
    
    * update yaml

---
+++ packages/orchestration/src/client/api/schema/completion-post-response-streaming.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import type { ModuleResults } from './module-results.js';
+import type { LLMModuleResultStreaming } from './llm-module-result-streaming.js';
+/**
+ * Representation of the 'CompletionPostResponseStreaming' schema.
+ */
+export type CompletionPostResponseStreaming = {
+  /**
+   * ID of the request
+   */
+  request_id: string;
+  module_results?: ModuleResults;
+  orchestration_result?: LLMModuleResultStreaming;
+} & Record<string, any>;
---
+++ packages/orchestration/src/client/api/schema/llm-module-result-synchronous.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import type { LlmChoice } from './llm-choice.js';
+import type { TokenUsage } from './token-usage.js';
+/**
+ * Output of LLM module. Follows the OpenAI spec.
+ */
+export type LLMModuleResultSynchronous = {
+  /**
+   * ID of the response
+   * @example "chatcmpl-9rO0aLoPKY7RtqkWi1317bazHEVFr"
+   */
+  id: string;
+  /**
+   * Object type
+   * @example "chat.completion"
+   */
+  object: string;
+  /**
+   * Unix timestamp
+   * @example 1722510700
+   */
+  created: number;
+  /**
+   * Model name
+   * @example "gpt-4"
+   */
+  model: string;
+  /**
+   * System fingerprint
+   * @example "fp_44709d6fcb"
+   */
+  system_fingerprint?: string;
+  /**
+   * Choices
+   */
+  choices: LlmChoice[];
+  usage: TokenUsage;
+} & Record<string, any>;
