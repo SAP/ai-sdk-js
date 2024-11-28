5533a6fd7e132ea7a90c7b2c6b1239581ed7a344
Junjie Tang <junjie.tang@sap.com>
2024-11-28 12:05:36 +0100

    Revert "Update packages/rage/src/index.ts"
    This reverts commit 36414c74e346c126068e1c87333398815bddd800.

--- packages/rage/src/index.ts
+++ packages/rage/src/index.ts
@@ -1,1 +1,3 @@
 export * from './client/pipeline/pipeline-api/index.js';
+export * as VectorApi from './client/vector/vector-api/index.js';
+export * as RetrievalApi from './client/retrieval/retrieval-api/index.js';
