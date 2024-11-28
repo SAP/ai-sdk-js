36414c74e346c126068e1c87333398815bddd800
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2024-11-28 11:59:13 +0100

    Update packages/rage/src/index.ts

--- packages/rage/src/index.ts
+++ packages/rage/src/index.ts
@@ -1,3 +1,1 @@
 export * from './client/pipeline/pipeline-api/index.js';
-export * as VectorApi from './client/vector/vector-api/index.js';
-export * as RetrievalApi from './client/retrieval/retrieval-api/index.js';
