330c1a3d5efafb0e0aebaf5e0d3f457cf035656e
cloud-sdk-js <cloud-sdk-js@github.com>
2025-01-03 13:21:47 +0000

    fix: Changes from lint

--- packages/orchestration/src/index.ts
+++ packages/orchestration/src/index.ts
@@ -43,7 +43,10 @@ export type {
 
 export { OrchestrationClient } from './orchestration-client.js';
 
-export { buildAzureContentFilter, modelParamOptions } from './orchestration-utils.js';
+export {
+  buildAzureContentFilter,
+  modelParamOptions
+} from './orchestration-utils.js';
 
 export { OrchestrationResponse } from './orchestration-response.js';
 
