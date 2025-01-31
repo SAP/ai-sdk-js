a7448a08e8cd18c015ad4c6ce51e1d8f378dd8e3
Junjie Tang <33489572+jjtang1985@users.noreply.github.com>
2025-01-31 10:43:27 +0100

    Update deployment-resolver.ts (#515)

--- packages/ai-api/src/utils/deployment-resolver.ts
+++ packages/ai-api/src/utils/deployment-resolver.ts
@@ -127,7 +127,7 @@ export async function resolveDeploymentId(
 
   if (!deployments.length) {
     throw new Error(
-      'No deployment matched the given criteria: ' + JSON.stringify(opts)
+      `No deployment matched the given criteria: ${JSON.stringify(opts)}. Please make sure the deployment is successful, which is a prerequisite before consuming orchestration or founndation models.`
     );
   }
   return deployments[0].id;
