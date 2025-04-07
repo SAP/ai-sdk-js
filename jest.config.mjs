51ec32adfb7c61189c0ba229e31bd25dbb55c4a6
Zhongpin Wang <zhongpin.wang@sap.com>
2025-04-07 16:27:23 +0200

    fix: Warning in `ts-jest` regarding `isolatedModules` (#642)
    * fix: use mjs for global setup and support isolatedModules
    
    * fix: revert some unnecessary changes jest config
    
    * Update global-test-setup.mjs
    
    * Update global-test-teardown.mjs
    
    ---------
    
    Co-authored-by: Deeksha Sinha <88374536+deekshas8@users.noreply.github.com>

--- jest.config.mjs
+++ jest.config.mjs
@@ -59,10 +59,10 @@ const config = {
   // forceCoverageMatch: [],
 
   // A path to a module which exports an async function that is triggered once before all test suites
-  globalSetup: path.resolve(__dirname, 'global-test-setup.ts'),
+  globalSetup: path.resolve(__dirname, 'global-test-setup.mjs'),
 
   // A path to a module which exports an async function that is triggered once after all test suites
-  globalTeardown: path.resolve(__dirname, 'global-test-teardown.ts'),
+  globalTeardown: path.resolve(__dirname, 'global-test-teardown.mjs'),
 
   // A set of global variables that need to be available in all test environments
   // globals: {},
