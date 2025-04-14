b2be320f5561fb4211cd7aaca6f3fe9188daed73
KavithaSiva <32287936+KavithaSiva@users.noreply.github.com>
2025-04-14 14:23:53 +0200

    chore: Automate creating and merging documentation release notes during release (#644)

---
+++ scripts/get-package-version.test.ts
@@ -0,0 +1,27 @@
+import { resolve } from 'path';
+import mock from 'mock-fs';
+import { getPackageVersion } from './get-package-version.js';
+
+describe('get package version', () => {
+  afterEach(() => {
+    mock.restore();
+  });
+
+  it('returns the version of the package json in the same directory', async () => {
+    mock({
+      'package.json': '{ "version": "1.2.3" }'
+    });
+    expect(await getPackageVersion()).toEqual('1.2.3');
+  });
+
+  it('returns the version of a designated package json', async () => {
+    mock({
+      dir: {
+        'package.json': '{ "version": "4.5.6" }'
+      }
+    });
+    expect(await getPackageVersion(resolve('dir', 'package.json'))).toEqual(
+      '4.5.6'
+    );
+  });
+});
