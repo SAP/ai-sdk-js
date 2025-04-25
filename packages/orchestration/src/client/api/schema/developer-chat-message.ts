9a905423670a45990afb6d407470ea3efcd000d7
shibeshduw <shibesh.duwadi@sap.com>
2025-04-25 09:08:32 +0200

    spec update from orchestration/main

---
+++ packages/orchestration/src/client/api/schema/developer-chat-message.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import type { ChatMessageContent } from './chat-message-content.js';
+/**
+ * Representation of the 'DeveloperChatMessage' schema.
+ */
+export type DeveloperChatMessage = {
+  role: 'developer';
+  content: ChatMessageContent;
+};
