2afb534ec1dca522dc094a9e29a99512a6fcbb9d
Zhongpin Wang <zhongpin.wang@sap.com>
2025-07-23 17:11:53 +0200

    try fix again..

--- packages/prompt-registry/src/zod/index.ts
+++ packages/prompt-registry/src/zod/index.ts
@@ -1,2 +1,1 @@
-export * from './prompt-registry.zod.js';
 export { registryControllerPromptControllerCreateUpdatePromptTemplateBody as promptTemplatePostRequestSchema } from './prompt-registry.zod.js';
