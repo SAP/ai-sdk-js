eb81948472e1b2aaa4bf04d7028cfb9c5fc05221
M. Emin Cihangeri <m.emin.cihanger@sap.com>
2025-09-11 14:20:36 +0200

    chore: Fix type text deploymentId

--- tests/type-tests/test/orchestration.test-d.ts
+++ tests/type-tests/test/orchestration.test-d.ts
@@ -50,7 +50,7 @@ expectType<Promise<OrchestrationResponse>>(
         }
       }
     },
-    { deploymentId: 'deploymentId' } as any
+    { deploymentId: 'deploymentId' }
   ).chatCompletion()
 );
 
