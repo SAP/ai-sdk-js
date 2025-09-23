56cb79d8bd6e17578d29cc93deeeeac9cac89b24
Zhongpin Wang <zhongpin.wang@sap.com>
2025-09-23 14:53:14 +0200

    Auto fix lint, please fix this for me

--- packages/ai-api/src/index.ts
+++ packages/ai-api/src/index.ts
@@ -1,10 +1,5 @@
 export * from './client/AI_CORE_API/index.js';
 
-export type {
-  ModelDeployment,
-  DeploymentIdConfig,
-  ModelConfig,
-  ResourceGroupConfig
-} from './utils/index.js';
+export type { ModelDeployment, DeploymentIdConfig, ModelConfig, ResourceGroupConfig } from './utils/index.js';
 
 export { resolveDeploymentUrl } from './utils/index.js';
