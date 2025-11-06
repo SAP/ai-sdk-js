a6f7789f7db4284aca6feb78593b019fdfeda803
BrigittaK307 <brigitta.kovacs@sap.com>
2025-11-06 10:56:10 +0200

    Ran postgenerate script adapted to windows

--- packages/orchestration/src/client/api/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2025 SAP SE or an SAP affiliate company. All rights reserved.
- *
- * This is a generated file powered by the SAP Cloud SDK for JavaScript.
- */
-export * from './orchestration-completion-api.js';
-export * from './orchestration-embeddings-api.js';
-export * from './schema/index.js';
