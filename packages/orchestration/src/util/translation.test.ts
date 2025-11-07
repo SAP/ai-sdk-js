f9391a4e251099e15c574ebcbeb3639a48916982
BrigittaK307 <brigitta.kovacs@sap.com>
2025-11-07 10:59:10 +0200

    Solved failing test case

--- packages/orchestration/src/util/translation.test.ts
+++ packages/orchestration/src/util/translation.test.ts
@@ -38,35 +38,4 @@ describe('Translation module config', () => {
 
     expect(translationConfig).toEqual(expectedTranslationConfig);
   });
-
-  it('builds input translation config with applyTo', async () => {
-    const translationConfig = buildTranslationConfig('input', {
-      sourceLanguage: 'de-DE',
-      targetLanguage: 'en-US',
-      applyTo: [
-        {
-          category: 'placeholders',
-          items: ['user_input'],
-          source_language: 'de-DE'
-        }
-      ]
-    });
-
-    const expectedTranslationConfig: SAPDocumentTranslationInput = {
-      type: 'sap_document_translation',
-      config: {
-        source_language: 'de-DE',
-        target_language: 'en-US',
-        apply_to: [
-          {
-            category: 'placeholders',
-            items: ['user_input'],
-            source_language: 'de-DE'
-          }
-        ]
-      }
-    };
-
-    expect(translationConfig).toEqual(expectedTranslationConfig);
-  });
 });
