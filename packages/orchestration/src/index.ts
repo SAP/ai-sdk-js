b63575f476168f344c5917f5a7d37820cb123e52
BrigittaK307 <brigitta.kovacs@sap.com>
2025-12-04 14:26:50 +0200

    Added missing export to the index.ts

--- packages/orchestration/src/index.ts
+++ packages/orchestration/src/index.ts
@@ -32,6 +32,7 @@ export type {
   TranslationInputConfig,
   TranslationOutputConfig,
   DocumentTranslationApplyToSelector,
+  TranslationApplyToCategory,
   TranslationTargetLanguage,
   EmbeddingRequest,
   EmbeddingModelConfig,
