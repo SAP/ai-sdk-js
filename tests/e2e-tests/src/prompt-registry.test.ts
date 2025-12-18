75765679fac587d2c91daac2a74dea8160b26824
BrigittaK307 <brigitta.kovacs@sap.com>
2025-12-18 16:32:01 +0200

    Reverted template name to original one

--- tests/e2e-tests/src/prompt-registry.test.ts
+++ tests/e2e-tests/src/prompt-registry.test.ts
@@ -9,7 +9,7 @@ loadEnv();
 describe('prompt registry', () => {
   it('should create a prompt template and then delete', async () => {
     const { id } = await createPromptTemplate(
-      'ai-sdk-js-test-custom-resource-group-prompt-template',
+      'ai-sdk-js-e2e-test',
       'orchestration'
     );
     expect(id.length).toEqual(36);
