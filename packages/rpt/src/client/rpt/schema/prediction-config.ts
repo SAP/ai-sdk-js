bc6f9e267b7a1fc9795bc88c8eb6f6ed8c8735d9
Marika Marszalkowski <marika.marszalkowski@sap.com>
2026-01-13 09:26:39 +0100

    add generated client + workflows for rpt

---
+++ packages/rpt/src/client/rpt/schema/prediction-config.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2026 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import type { TargetColumnConfig } from './target-column-config.js';
+/**
+ * Configuration of the prediction model.
+ */
+export type PredictionConfig = {
+  target_columns: TargetColumnConfig[];
+};
