86ce89b86be7c13c644d031b038aaab5d1dee9be
Marika Marszalkowski <marika.marszalkowski@sap.com>
2026-03-11 08:19:10 +0100

    Apply suggestions from code review
    Co-authored-by: Marika Marszalkowski <marika.marszalkowski@sap.com>

--- sample-code/src/orchestration.ts
+++ sample-code/src/orchestration.ts
@@ -1035,15 +1035,14 @@ export async function orchestrationChatCompletionFile(
   fileType: FileType = 'pdf',
   options: {
     model?: string;
-  } = {}
+  } = { model: defaultModel }
 ): Promise<OrchestrationResponse> {
   const {
     filename,
     mimeType,
-    model: defaultModel,
+    model,
     instruction
   } = fileTypeConfig[fileType];
-  const model = options.model ?? defaultModel;
 
   const orchestrationClient = new OrchestrationClient({
     promptTemplating: {
