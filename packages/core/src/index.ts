420fd42e4b4ccd5c70ce3ce020a0681ae54de224
Marika Marszalkowski <marika.marszalkowski@sap.com>
2026-03-16 09:53:28 +0100

    chore: Extract embedding model for vertex AI

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,6 +6,7 @@ export type {
   AzureOpenAiChatModel,
   AzureOpenAiEmbeddingModel,
   GcpVertexAiChatModel,
+  GcpVertexAiEmbeddingModel,
   AwsBedrockChatModel,
   AiCoreOpenSourceChatModel,
   AiCoreOpenSourceEmbeddingModel,
