f4e636fec4d4cecc781e88b8674540f3c368f4b5
David Knaack <david.knaack@sap.com>
2026-07-28 10:25:52 +0200

    chore: fix workspace vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,18 @@
+import { join } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
     clearMocks: true,
-    snapshotSerializers: ['./scripts/vitest-snapshot-serializers.ts'],
+    snapshotSerializers: [join(import.meta.dirname, '/scripts/vitest-snapshot-serializers.ts')],
+    globalSetup: [join(import.meta.dirname, 'global-test-setup.mjs')],
     coverage: {
       provider: 'v8',
       reporter: ['html', 'lcov'],
     },
-    globalSetup: [new URL('./global-test-setup.mjs', import.meta.url).pathname],
+    projects: [
+      'packages/*/vitest.config.ts'
+    ],
   },
 });
