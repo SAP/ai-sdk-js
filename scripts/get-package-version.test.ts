84921b11e6c42d6bd9322a109728ff8c7824e757
David Knaack <david.knaack@sap.com>
2026-08-12 15:18:34 +0200

    chore: also migrate ai-sdk to memfs

--- scripts/get-package-version.test.ts
+++ scripts/get-package-version.test.ts
@@ -1,25 +1,23 @@
 import { resolve } from 'path';
-import mock from 'mock-fs';
+import { vi, describe, it, expect, beforeEach } from 'vitest';
+import { fs, vol } from 'memfs';
+
+vi.mock('fs/promises', () => fs.promises);
+
 import { getPackageVersion } from './get-package-version.ts';
 
 describe('get package version', () => {
-  afterEach(() => {
-    mock.restore();
+  beforeEach(() => {
+    vol.reset();
   });
 
   it('returns the version of the package json in the same directory', async () => {
-    mock({
-      'package.json': '{ "version": "1.2.3" }'
-    });
+    vol.fromJSON({ 'package.json': '{ "version": "1.2.3" }' });
     expect(await getPackageVersion()).toEqual('1.2.3');
   });
 
   it('returns the version of a designated package json', async () => {
-    mock({
-      dir: {
-        'package.json': '{ "version": "4.5.6" }'
-      }
-    });
+    vol.fromJSON({ [resolve('dir', 'package.json')]: '{ "version": "4.5.6" }' });
     expect(await getPackageVersion(resolve('dir', 'package.json'))).toEqual(
       '4.5.6'
     );
