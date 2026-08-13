ab47867d811c52defa60dc8d7a7ae032bde18c56
David Knaack <david.knaack@sap.com>
2026-08-13 10:49:08 +0200

    chore: additional cleanup

--- scripts/get-package-version.test.ts
+++ scripts/get-package-version.test.ts
@@ -1,4 +1,3 @@
-import { resolve } from 'path';
 import { vi, describe, it, expect, afterEach } from 'vitest';
 import { fs, vol } from 'memfs';
 
@@ -13,12 +12,12 @@ describe('get package version', () => {
 
   it('returns the version of the package json in the same directory', async () => {
     vol.fromJSON({ 'package.json': '{ "version": "1.2.3" }' });
-    expect(await getPackageVersion()).toEqual('1.2.3');
+    await expect(getPackageVersion()).resolves.toEqual('1.2.3');
   });
 
   it('returns the version of a designated package json', async () => {
-    vol.fromJSON({ [resolve('dir', 'package.json')]: '{ "version": "4.5.6" }' });
-    expect(await getPackageVersion(resolve('dir', 'package.json'))).toEqual(
+    vol.fromJSON({ 'dir/package.json': '{ "version": "4.5.6" }' });
+    await expect(getPackageVersion('dir/package.json')).resolves.toEqual(
       '4.5.6'
     );
   });
