caeb4c32dff44d6849cade3d9bbae750c6a96fd3
i341658 <kavitha.sivakumar@sap.com>
2026-09-15 22:10:30 +0200

    feat(ci): Migrate GitHub Actions from cloud-sdk and update pnpm version

---
+++ build-packages/get-changelog/lib/build-packages/get-changelog/index.js
@@ -0,0 +1,21 @@
+import { readFileSync } from 'node:fs';
+import { readFile } from 'node:fs/promises';
+import { setOutput, setFailed } from '@actions/core';
+async function getPackageVersion() {
+    const packageJson = await readFile('package.json', 'utf8');
+    return JSON.parse(packageJson).version;
+}
+async function getChangelog(v) {
+    const changelog = readFileSync('CHANGELOG.md', { encoding: 'utf8' });
+    const [, olderLogs] = changelog.split(`\n# ${v || (await getPackageVersion())}`);
+    const logs = olderLogs.split('\n# ')[0];
+    return logs.slice(logs.indexOf('\n##') + 1);
+}
+(async () => {
+    try {
+        setOutput('changelog', await getChangelog());
+    }
+    catch (error) {
+        setFailed(error.message);
+    }
+})();
